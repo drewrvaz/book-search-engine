eb6c3ff311c1b76c64f56229aa87058bce358044
drewrvaz <drewrvaz@gmail.com>
2022-12-19 12:26:58 -0500

    Still torubleshooting some issues. Hopefully will deploy to Heroku now

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -42,7 +42,7 @@ const SignupForm = () => {
         variables: { ...userFormData },
       });
 
-      Auth.login(data.login.token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
     }
